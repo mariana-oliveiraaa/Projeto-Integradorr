1ceb6e8027357f12988022527850d04ceff6b5bd
mariana-oliveiraaa <99214209+mariana-oliveiraaa@users.noreply.github.com>
2022-05-09 15:07:19 -0300

    Add files via upload

---
+++ ProjetoIntegrador-vue/src/components/MenuLateral.vue
@@ -0,0 +1,15 @@
+<script>
+export default {};
+</script>
+<template>
+  <aside id="sidenav">
+    <ul>
+      <li>Licenciatura</li>
+      <li>Bacharelado</li>
+      <li>Tecnólogo</li>
+     
+    </ul>
+    <h1></h1>
+  </aside>
+</template>
+<style></style>
